cd949c4d8ce21afe9ae1b1a2de3e5ae2bb8a9940
aWillard-13 <awillard@my.ccsu.edu>
2025-05-03 22:53:37 -0400

    Update HumidityChecker.py
    Added some logic for the links not to steal the browser.

--- HumidityChecker.py
+++ HumidityChecker.py
@@ -1,28 +1,20 @@
 # Andrew Willard
-# 20250503
+# 20250415
 # Humidity Checker
 # MIS310
 
-"""
-base logic is:
-if the humidity is below 40%, turn on the high-intensity misting system, waits 3 minutes, checks the humidity again.
-If the humidity is  40% - 50%, turn on the low-intensity misting system, waits 3 minutes, checks the humidity again.
-If the humidity above 50%, waits 10 minutes, checks the humidity again.
-"""
-
-# libs
 import time
 import random
 import webbrowser
 
-# adds a real typing style to a string
+
+# Adds a real typing style to a string
 def realistic_typing(text):
-    for char in text:
-        print(char, end='', flush=True)
-        time.sleep(random.uniform(0.05, 0.1))
+    for char in text: print(char, end='', flush=True); time.sleep(random.uniform(0.05, 0.1))
     print()
 
-# check yes or no
+
+# Check yes or no
 def ask_yes_no(question):
     while True:
         answer = input(question).strip().lower()
@@ -31,31 +23,43 @@ def ask_yes_no(question):
         elif answer == "": realistic_typing("Oh, I see what you did there. No, read the line before you just press enter this time.")
         else: realistic_typing("It's a yes or no question...")
 
-# I couldn't resist...
-def open_link_and_prompt(url):
-    webbrowser.open(url)
-    input(f"The link has been opened in your browser: {url}\nPress [ENTER] when you're ready to continue. ")
+
+# Conditionally open a link, based on user preference
+def open_link_and_prompt(url, allow_links):
+    if allow_links: webbrowser.open(url); input(f"The link has been opened in your browser: {url}\nPress [ENTER] to continue. ")
+    else: print(url)
+
 
 # Narration section 1
-narration1 = [
-    "ok. so here is the thing...",
-    "In order to get this program to do what we want it to do,",
-    "there are a few things we need to do first.",
-    "first and foremost, are you ok with this fun and fancy automation thing?",
-    "or perhaps you'd prefer pressing enter at every line in the boring old fashion......"
-]
+narration1 = [  "ok. so here is the thing...",
+                "In order to get this program to do what we want it to do,",
+                "there are a few things we need to do -",
+                "First and foremost, are you ok with this fun and fancy automation thing?",
+                "or perhaps you'd prefer pressing enter at every line in the boring old fashion way....."
+                ]
 
 for msg in narration1: realistic_typing(msg); time.sleep(1)
 
-# Ask user about automation
-manual = ask_yes_no("Would you rather manually advance the messages? (yes/no): ")
+# First decision: manual or auto
+first_manual_choice = ask_yes_no("Would you rather manually advance the messages? (yes/no): ")
 
-narration2 = ["testing...", "1...", "2...", "3."]
+narration2 = ["testing...", "1...", "2...", "3...", "boom."]
+
 for msg in narration2:
-    if manual: print(msg); input("[Press ENTER to continue]")
+    if first_manual_choice: print(msg); input("[ENTER] to continue")
     else: realistic_typing(msg); time.sleep(1)
 
-# Narration section 3
+webbrowser.open("https://youtu.be/mVZSsDYBPIs?si=CAC5vpx2HzH80Ega")
+
+# Ask about links
+allow_links = ask_yes_no("Would you like me to open links in your browser? (yes/no): ")
+
+if not allow_links:
+    realistic_typing("too bad!")
+    webbrowser.open("https://www.youtube.com/watch?v=J---aiyznGQ") # cat
+    realistic_typing("this is a joke, we will respect your choice here on out... for the most part.")
+
+# Second narration
 narration3 = [
     "We have replaced the gardener with automation.",
     "Go buy an Adafruit HTS221 - Temperature & Humidity Sensor.",
@@ -74,7 +78,7 @@ narration3 = [
     "https://www.adafruit.com/circuitpython",
     "Then add the following code to the board:",
     '''
-        
+
     # added for dwell time.
     import time 
     # used to interact with the micro controller and the pin outs.
@@ -82,43 +86,43 @@ narration3 = [
     import digitalio   # https://docs.circuitpython.org/en/latest/shared-bindings/digitalio/
     # Include the library for the built-in HTS221 temperature and humidity sensor
     import adafruit_hts221   # https://www.adafruit.com/product/4535
-    
+
     # Set up I2C and the sensor to work with our board...
     i2c = board.I2C()
     sensor = adafruit_hts221.HTS221(i2c)
-    
+
     # Set up relays...
     mist_high = digitalio.DigitalInOut(board.D2)
     mist_high.direction = digitalio.Direction.OUTPUT
     mist_low = digitalio.DigitalInOut(board.D3)
     mist_low.direction = digitalio.Direction.OUTPUT
-    
+
     # get two relays like this - https://www.adafruit.com/product/3191
     # connect them to a misting system... 
     # I already learned more than I planned to going into this about misting systems, zip drip, and wet bulb index...
     # I trust you to google misting system and to buy something under $50.
-    
+
     # Flag to skip the 10-minute wait the first time
     first_run = True
-    
+
     # program loop. goes for ever and ever.
     while True:
-    humidity = sensor.relative_humidity
+        humidity = sensor.relative_humidity
 
-    if humidity < 40:
-        mist_high.value = True
-        time.sleep(180)
-        mist_high.value = False
+        if humidity < 40:
+            mist_high.value = True
+            time.sleep(180)
+            mist_high.value = False
 
-    elif humidity < 50:
-        mist_low.value = True
-        time.sleep(180)
-        mist_low.value = False
-    else:
-        if not first_run:
-            time.sleep(600)
+        elif humidity < 50:
+            mist_low.value = True
+            time.sleep(180)
+            mist_low.value = False
+        else:
+            if not first_run:
+                time.sleep(600)
 
-    first_run = False ''',
+        first_run = False ''',
 
     "add a floating toilet valve to your water tank to get it to refill when the water level is low,",
     "and you don't even need to pay someone to check the level of the tank.",
@@ -129,16 +133,20 @@ narration3 = [
     "https://www.amazon.com/MistKing-Hygrostat-Thermometer-Humidity-Control/dp/B074HLXM46?ref_=ast_sto_dp"
 ]
 
-# Ask user again about narration style
-manual = ask_yes_no("So, are you happy with your choice? (yes/no): ")
+# Second decision: are they still happy with that manual/auto choice?
+second_answer = ask_yes_no("So, are you happy with your choice? (yes/no): ")
+
+# Reverse the manual mode if they said no to their first choice
+manual_mode = first_manual_choice
+if not second_answer: manual_mode = not manual_mode
 
 for msg in narration3:
-    if msg.strip().startswith("http"): open_link_and_prompt(msg.strip())
+    if msg.strip().startswith("http"): open_link_and_prompt(msg.strip(), allow_links)
     else:
-        if manual: print(msg); input("Press [ENTER] to continue...")
+        if manual_mode: print(msg); input("Press [ENTER] to continue.")
         else: realistic_typing(msg); time.sleep(1)
 
 realistic_typing("well that was fun! let's do that again sometime.")
 
 realistic_typing("And because I never promised I wouldn't use my powers for evil... ")
-open_link_and_prompt("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+open_link_and_prompt("https://www.youtube.com/watch?v=dQw4w9WgXcQ", True)
